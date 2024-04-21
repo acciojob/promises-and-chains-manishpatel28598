5bed3a89e0e53abec42d75d1141147e7ffedd21c
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-04-21 19:20:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //your JS code here. If required.
 const btn = document.getElementById("btn");
-const name1 = document.getElementById("name");
-const age = document.getElementById("age");
-
+let name1 = document.getElementById("name");
+let age = document.getElementById("age");
+console.log(age.value);
+console.log(name1.value);
 
 function validate(){
+	
   return new Promise((resolve, reject)=>{
       setTimeout(()=>{
 		  if(age.value>18){
-		        resolve(`Welcome, ${name1.value} You can vote`);
+		        resolve(`Welcome, ${name1.value}. You can vote.`);
 		  }
 		  else{
 			   reject(`Oh sorry ${name1.value}. You aren't old enough.`);
@@ -17,8 +19,14 @@ function validate(){
   })
 }
 
-async function displayMsg(){
-  const result = await validate().then((el)=>alert(el)).catch((error)=>alert(error));
+
+function displayMsg(){
+ 
+	if (age.value==="" && name1.value==="") {
+    alert("Please enter valid details");
+    return;
+  }
+  const result = validate().then((el)=>alert(el)).catch((error)=>alert(error));
 }
 
 btn.addEventListener('click', displayMsg);
